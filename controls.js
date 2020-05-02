822fde1c6d1c7cf90ee88992e911042fd8c83b4a
Marcio Junior <marciojunior_eu@yahoo.com.br>
2020-05-02 11:49:05 +0800

    Game working

--- controls.js
+++ controls.js
@@ -1,48 +1,19 @@
-const controlMappings = [
-  {
-    w: "up",
-    s: "down",
-    " ": "start",
-  },
-  {
-    ArrowUp: "up",
-    ArrowDown: "down",
-    Enter: "start",
-  },
-];
-const controls = [{}, {}];
+const controlMappings = {
+  ArrowLeft: "left",
+  ArrowRight: "right",
+  ArrowDown: "down",
+  " ": "rotate",
+};
+const controls = {};
 
 document.addEventListener("keydown", (evt) => {
-  controls.forEach((control, i) => {
-    control[controlMappings[i][evt.key]] = true;
-  });
+  controls[controlMappings[evt.key]] = true;
 });
 
 document.addEventListener("keyup", (evt) => {
-  controls.forEach((control, i) => {
-    control[controlMappings[i][evt.key]] = false;
-  });
+  controls[controlMappings[evt.key]] = false;
 });
 
-function getGamePadControl(index) {
-  const gamepad = navigator.getGamepads()[index];
-  if (gamepad) {
-    const axes = gamepad.axes;
-    return {
-      start: gamepad.buttons[4].pressed,
-      up: axes[0] > 0.2,
-      down: axes[1] > 0.2,
-    };
-  }
-}
-
-function getKeyBoardControl(index) {
-  return Object.assign({}, controls[index]);
-}
-
 export default function getControls() {
-  return [
-    getGamePadControl(0) || getKeyBoardControl(0),
-    getGamePadControl(1) || getKeyBoardControl(1),
-  ];
+  return Object.assign({}, controls);
 }
--- game.js
+++ game.js
@@ -1,3 +1,59 @@
+import getControls from "./controls.js";
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sample(array) {
+  return array[rand(0, array.length - 1)];
+}
+
+// prettier-ignore
+const tetriminos = [
+  [
+    0, 0, 2, 0,
+    0, 2, 2, 0,
+    0, 2, 0, 0,
+    0, 0, 0, 0,
+  ],
+  [
+    0, 3, 0, 0,
+    0, 3, 3, 0,
+    0, 0, 3, 0,
+    0, 0, 0, 0,
+  ],
+  [
+    0, 4, 4, 0,
+    0, 4, 0, 0,
+    0, 4, 0, 0,
+    0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0,
+    0, 5, 5, 0,
+    0, 5, 5, 0,
+    0, 0, 0, 0,
+  ],
+  [
+    0, 6, 6, 0,
+    0, 0, 6, 0,
+    0, 0, 6, 0,
+    0, 0, 0, 0,
+  ],
+  [
+    0, 0, 7, 0,
+    0, 7, 7, 0,
+    0, 0, 7, 0,
+    0, 0, 0, 0,
+  ],
+  [
+    0, 0, 8, 0,
+    0, 0, 8, 0,
+    0, 0, 8, 0,
+    0, 0, 8, 0,
+  ]
+]
+
 export default class Game {
   constructor(canvas) {
     canvas.style.border = "1px solid black";
@@ -5,15 +61,40 @@ export default class Game {
     this.cols = 12;
     this.rows = 22;
     this.board = Array(this.rows * this.cols).fill(0);
-    this.colors = ["#ffffff", "#000000"];
-    this.forEachBlock((x, y, i) => {
-      if (x === 0 || x === this.cols - 1) {
-        this.board[i] = 1;
+    this.colors = [
+      null,
+      "#666666",
+      "#ff0000",
+      "#00ff00",
+      "#0000ff",
+      "#ffff00",
+      "#ffa500",
+      "#800080",
+      "#00ffff",
+    ];
+
+    for (let x = 0; x < this.cols; x++) {
+      for (let y = 0; y < this.rows; y++) {
+        const i = y * this.cols + x;
+        if (x === 0 || x === this.cols - 1 || y === this.rows - 1) {
+          this.board[i] = 1;
+        }
       }
-      if (y === 0 || y === this.rows - 1) {
-        this.board[i] = 1;
-      }
-    });
+    }
+
+    this.newTetrimino();
+    this.prevControls = getControls();
+    this.holdThreshold = 0;
+    this.ticks = 0;
+    this.state = "playing";
+  }
+  newTetrimino() {
+    this.player = {
+      x: this.cols / 2 - 1,
+      y: 0,
+      rotation: 0,
+      tetrimino: sample(tetriminos),
+    };
   }
   get width() {
     return this.ctx.canvas.width;
@@ -24,24 +105,173 @@ export default class Game {
   clear() {
     this.ctx.clearRect(0, 0, this.width, this.height);
   }
-  update() {}
-  render() {
-    this.forEachBlock((x, y, i) => {
-      this.renderBlock(x, y, this.board[i]);
-    });
+  collide(x, y, rotation) {
+    const { tetrimino } = this.player;
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        const px = x + col;
+        const py = y + row;
+        if (
+          tetrimino[this.rotate(col, row, rotation)] !== 0 &&
+          this.board[py * this.cols + px]
+        ) {
+          return true;
+        }
+      }
+    }
   }
-  forEachBlock(cb) {
-    for (let i = 0; i < this.cols * this.rows; i++) {
-      const x = i % this.cols;
-      const y = Math.floor(i / this.cols);
-      cb(x, y, i);
+  removeFilledLines() {
+    nextline: for (let row = this.rows - 2; row >= 0; row--) {
+      for (let col = this.cols - 1; col >= 0; col--) {
+        if (this.board[row * this.cols + col] === 0) {
+          continue nextline;
+        }
+      }
+
+      // The current row should be removed
+      this.board.splice(row * this.cols, this.cols);
+
+      // Prepend a new line at the beginning
+      for (let col = 0; col < this.cols; col++) {
+        if (col === 0 || col === this.cols - 1) {
+          this.board.unshift(1);
+        } else {
+          this.board.unshift(0);
+        }
+      }
+    }
+  }
+  update() {
+    const { prevControls, player } = this;
+    const controls = getControls();
+
+    if (this.state === "playing") {
+      this.removeFilledLines();
+
+      if (this.collide(player.x, player.y, player.rotation)) {
+        this.state = "gameover";
+        return;
+      }
+
+      if (controls.left || controls.right || controls.down || controls.rotate) {
+        if (this.holdThreshold % 5 === 0) {
+          if (controls.left) {
+            if (!this.collide(player.x - 1, player.y, player.rotation)) {
+              player.x--;
+            }
+          } else if (controls.right) {
+            if (!this.collide(player.x + 1, player.y, player.rotation)) {
+              player.x++;
+            }
+          } else if (controls.down) {
+            if (this.collide(player.x, player.y + 1, player.rotation)) {
+              this.lockTetrimino();
+              this.newTetrimino();
+            } else {
+              player.y++;
+            }
+          }
+        }
+        if (this.holdThreshold % 10 === 0) {
+          if (controls.rotate) {
+            if (!this.collide(player.x, player.y, player.rotation + 1)) {
+              player.rotation++;
+            }
+          }
+        }
+        this.holdThreshold++;
+      }
+
+      if (
+        (prevControls.left && !controls.left) ||
+        (prevControls.right && !controls.right) ||
+        (prevControls.down && !controls.down) ||
+        (prevControls.start && !controls.start) ||
+        (prevControls.rotate && !controls.rotate)
+      ) {
+        this.holdThreshold = 0;
+      }
+
+      this.ticks++;
+      if (this.ticks % 30 === 0) {
+        if (this.collide(player.x, player.y + 1, player.rotation)) {
+          this.lockTetrimino();
+          this.newTetrimino();
+        } else {
+          this.player.y++;
+        }
+      }
+
+      this.prevControls = controls;
+    }
+  }
+  lockTetrimino() {
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        const { x, y, tetrimino, rotation } = this.player;
+        const px = x + col;
+        const py = y + row;
+        const i = py * this.cols + px;
+        const color = tetrimino[this.rotate(col, row, rotation)];
+        if (color !== 0) {
+          this.board[i] = color;
+        }
+      }
+    }
+  }
+  renderGameOver() {
+    this.ctx.fillStyle = "#000000";
+    this.ctx.font = "20px Gameplay";
+    this.ctx.fillText("Game Over", this.width / 2 - 60, this.height / 2);
+  }
+  render() {
+    this.renderBoard();
+    this.renderPlayer();
+    if (this.state === "gameover") {
+      this.renderGameOver();
+    }
+  }
+  renderPlayer() {
+    for (let row = 0; row < 4; row++) {
+      for (let col = 0; col < 4; col++) {
+        const { x, y, tetrimino, rotation } = this.player;
+        this.renderBlock(
+          x + col,
+          y + row,
+          tetrimino[this.rotate(col, row, rotation)]
+        );
+      }
+    }
+  }
+  rotate(x, y, rotation) {
+    switch (rotation % 4) {
+      case 0:
+        return y * 4 + x;
+      case 1:
+        return 12 + y - 4 * x;
+      case 2:
+        return 15 - x - 4 * y;
+      case 3:
+        return 3 - y + 4 * x;
+    }
+  }
+  renderBoard() {
+    for (let col = 0; col < this.cols; col++) {
+      for (let row = 0; row < this.rows; row++) {
+        this.renderBlock(col, row, this.board[row * this.cols + col]);
+      }
     }
   }
   renderBlock(x, y, colorIndex) {
+    if (colorIndex === 0) return;
     const blockWidth = this.width / this.cols;
     const blockHeight = this.height / this.rows;
     this.ctx.fillStyle = this.colors[colorIndex];
     this.ctx.fillRect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+    this.ctx.fillStyle = "#000000";
+    this.ctx.beginPath();
+    this.ctx.rect(x * blockWidth, y * blockHeight, blockWidth, blockHeight);
+    this.ctx.stroke();
   }
   nextFrame() {
     this.clear();
